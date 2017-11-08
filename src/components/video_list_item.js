352b46a12fce5133dfb9f5d8651e0ac77e809060
Alastair Moore <welly@protonmail.com>
2017-11-08 21:04:42 +0000

    Changed component to functional component, finished off component output with video result item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
-class VideoListItem extends Component {
-	render() {
-		return (
-			<li>Video</li>
-		);
-	}
+const VideoListItem = ({video, onVideoSelect}) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+	return (
+		<li className="list-group-item" onClick={() => onVideoSelect(video)}>
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title}</div>
+				</div>
+			</div>
+		</li>
+	);
 }
 
 export default VideoListItem;
