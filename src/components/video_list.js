1fc154c186fcb827589de96df3897c7c592333ed
Alastair Moore <welly@protonmail.com>
2017-11-08 21:03:24 +0000

    Changed component to functional component. Passed props down the tree

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import VideoListItem from './video_list_item';
 
-class VideoList extends Component {
-	render() {
-		const videoItems = this.props.videos.map((video) => {
-			return <VideoListItem key={video.etag} video={video} />
-		});
+const VideoList = (props) => {
+	const videoItems = props.videos.map((video) => {
 		return (
-			<ul className="col-md-4 list-group">
-				{videoItems}
-			</ul>
-		);		
-	}
+			<VideoListItem 
+				key={ video.etag } 
+				onVideoSelect={ props.onVideoSelect }
+				video={ video } 
+			/>
+		);
+	});
+	return (
+		<ul className="col-md-4 list-group">
+			{videoItems}
+		</ul>
+	);		
 }
 
 export default VideoList;
-
