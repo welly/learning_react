471e75a9af37597e9e88f61ed696246fc67c5c63
Alastair Moore <welly@protonmail.com>
2017-11-08 21:03:35 +0000

    Handling passed property function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,17 +9,18 @@ class SearchBar extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 				<input 
 					value={this.state.term}
-					onChange={(event) => this.setState({term: event.target.value})} 
+					onChange={event => this.onInputChange(event.target.value)} 
 				/>
 			</div>
 		);
 	}
 
-	onInputChange(event) {
-		console.log(event.target.value);
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
